58359986929c95954084ba9020fab431c79956e2
Revanth2213 <136300933+Revanth2213@users.noreply.github.com>
2025-09-29 14:15:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,43 +20,28 @@ resource "aws_vpc" "my-vpc" {
   }
 }
 
-/* Create Web Public Subnet
-resource "aws_subnet" "web-subnet-1" {
+# Create Web Public Subnet
+resource "aws_subnet" "subnet-1" {
   vpc_id                  = aws_vpc.my-vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-west-2a"
   map_public_ip_on_launch = true
-
-  tags = {
-    Name = "Web-1a"
-  }
-}*/
-
-/*resource "aws_subnet" "web-subnet-2" {
-  vpc_id                  = aws_vpc.my-vpc.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-west-2b"
-  map_public_ip_on_launch = true
- tags = {
-    Name = "Web-lb"
   }
 }
-*/
 
 
-/* Create Application Private Subnet
-resource "aws_subnet" "application-subnet-1" {
+
+# Create  Private Subnet
+resource "aws_subnet" "Private-subnet-2" {
   vpc_id                  = aws_vpc.my-vpc.id
-  cidr_block              = "10.0.11.0/24"
+  cidr_block              = "10.0.3.0/24"
   availability_zone       = "us-west-2a"
   map_public_ip_on_launch = false
 
-  tags = {
-    Name = "Application-1a"
-  }
-}*/
-
-resource "aws_subnet" "application-subnet-2" {
+  
+}
+#public Subnet
+resource "aws_subnet" "application-subnet-1" {
   vpc_id                  = aws_vpc.my-vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-west-2b"
@@ -73,29 +58,10 @@ resource "aws_subnet" "database-subnet-1" {
   availability_zone = "us-west-2a"
 
   tags = {
-    Name = "Database-private-1a"
+    Name = "database-subnet-1a"
   }
 }
 
-/*resource "aws_subnet" "database-subnet-2" {
-  vpc_id            = aws_vpc.my-vpc.id
-  cidr_block        = "10.0.22.0/24"
-  availability_zone = "us-west-2b"
-
-  tags = {
-    Name = "Database-lb"
-  }
-}*/
-
-resource "aws_subnet" "database-subnet" {
-  vpc_id            = aws_vpc.my-vpc.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-west-2a"
-
-  tags = {
-    Name = "Database"
-  }
-}
 
 # Create Internet Gateway
 resource "aws_internet_gateway" "igw" {
@@ -113,7 +79,7 @@ resource "aws_route_table" "web-rt" {
 
 
   route {
-    cidr_block = "172.16.0.0/0" 
+    cidr_block = "0.0.0.0/0" 
     gateway_id = aws_internet_gateway.igw.id
   }
 
@@ -122,45 +88,8 @@ resource "aws_route_table" "web-rt" {
   }
 }
 
-# Create Web Subnet association with Web route table
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.web-subnet-1.id
-  route_table_id = aws_route_table.web-rt.id
-}
 
-resource "aws_route_table_association" "b" {
-  subnet_id      = aws_subnet.web-subnet-2.id
-  route_table_id = aws_route_table.web-rt.id
-}
 
-/*#Create EC2 Instance
-resource "aws_instance" "webserver1" {
-  ami                    = "ami-0c80e2b6ccb9ad6d1"
-  instance_type          = "t2.micro"
-  availability_zone      = "us-west-2a"
-  key_name               = "terraform"
-  vpc_security_group_ids = [aws_security_group.webserver-sg.id]
-  subnet_id              = aws_subnet.web-subnet-1.id
-  user_data              = "${file("apache.sh")}"
-
-  tags = {
-    Name = "Web Server-1"
-  }
-}*/
-
-/*resource "aws_instance" "webserver2" {
-  ami                    = "ami-0c80e2b6ccb9ad6d1"
-  instance_type          = "t2.micro"
-  availability_zone      = "us-west-2b"
-  key_name               = "terraform"
-  vpc_security_group_ids = [aws_security_group.webserver-sg.id]
-  subnet_id              = aws_subnet.web-subnet-2.id
-  user_data              = "${file("apache.sh")}"
-
-  tags = {
-    Name = "Web Server-2"
-  }
-}*/
 
 #Create EC2 Instance
 resource "aws_instance" "appserver1" {
@@ -169,24 +98,11 @@ resource "aws_instance" "appserver1" {
   availability_zone      = "us-west-2a"
   key_name               = "terraform"
   vpc_security_group_ids = [aws_security_group.appserver-sg.id]
-  subnet_id              = aws_subnet.application-subnet-1.id
   tags = {
     Name = "app Server-1"
   }
 }
 
-/*resource "aws_instance" "appserver2" {
-  ami                    = "ami-0c80e2b6ccb9ad6d1"
-  instance_type          = "t2.micro"
-  availability_zone      = "us-west-2b"
-  key_name               = "terraform"
-  vpc_security_group_ids = [aws_security_group.appserver-sg.id]
-  subnet_id              = aws_subnet.application-subnet-2.id
-
-  tags = {
-    Name = "app Server-2"
-  }
-}*/
 
 resource "aws_db_instance" "default" {
   allocated_storage    = 10
@@ -201,7 +117,7 @@ resource "aws_db_instance" "default" {
 
 resource "aws_db_subnet_group" "default" {
   name       = "main"
-  subnet_ids = [aws_subnet.database-subnet-1.id, aws_subnet.database-subnet-2.id]
+  subnet_ids = [aws_subnet.database-subnet-1.id, aws_subnet.database-subnet-1.id]
 
   tags = {
     Name = "My DB subnet group"
@@ -209,39 +125,7 @@ resource "aws_db_subnet_group" "default" {
 }
 
 
-/*# Create Web Security Group
-resource "aws_security_group" "webserver-sg" {
-  name        = "webserver-sg"
-  description = "Allow HTTP inbound traffic"
-  vpc_id      = aws_vpc.my-vpc.id
 
-  ingress {
-    description = "HTTP from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTP from VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
- tags = {
-    Name = "Web-SG"
-  }
-}*/
-6
 # Create Application Security Group
 resource "aws_security_group" "appserver-sg" {
   name        = "appserver-SG"
@@ -301,62 +185,6 @@ resource "aws_security_group" "database-sg" {
     Name = "Database-SG"
   }
 }
-
-resource "aws_lb" "external-elb" {
-  name               = "External-LB"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.webserver-sg.id]
-  subnets            = [aws_subnet.web-subnet-1.id, aws_subnet.web-subnet-2.id]
-}
-
-resource "aws_lb_target_group" "external-elb" {
-  name     = "ALB-TG"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.my-vpc.id
-}
-
-resource "aws_lb_target_group_attachment" "external-elb1" {
-  target_group_arn = aws_lb_target_group.external-elb.arn
-  target_id        = aws_instance.webserver1.id
-  port             = 80
-
-  depends_on = [
-    aws_instance.webserver1,
-  ]
-}
-
-resource "aws_lb_target_group_attachment" "external-elb2" {
-  target_group_arn = aws_lb_target_group.external-elb.arn
-  target_id        = aws_instance.webserver2.id
-  port             = 80
-
-  depends_on = [
-    aws_instance.webserver2,
-  ]
-}
-
-resource "aws_lb_listener" "external-elb" {
-  load_balancer_arn = aws_lb.external-elb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.external-elb.arn
-  }
-}
-
-
-
-
-output "lb_dns_name" {
-  description = "The DNS name of the load balancer"
-  value       = aws_lb.external-elb.dns_name
-}
-
-
 resource "aws_s3_bucket" "example" {
   bucket = "rahamtestbycketterra7788abcdefxxc54hj6jfegfefgrg"
 
@@ -378,5 +206,5 @@ default = ["userone", "usertwo", "userthree", "userfour"]
 }
 
 resource "aws_iam_group" "two" {
-name = "devopswithawsbyrahamshaik"
+name = "terraform"
 }
